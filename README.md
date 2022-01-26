f7d52401690ac8e0a5f2a34b4fc156dca3972059
lukasz-glen <>
2022-01-26 12:52:52 +0100

    update

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # CryptoKittiesMock
-The mock of non-standard NFT for transfer purposes
+
+This is the mock of non-standard NFT for transfer purposes.
+It has functionality required for tests and integration tests only.
+
+It should not be used on the production.
+
+## Install and run
+
+```shell
+npm install
+
+npx hardhat compile
+
+npx hardhat test
+```
+
--- contracts/CryptoKittiesMock.sol
+++ contracts/CryptoKittiesMock.sol
@@ -1,8 +1,9 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity 0.8.0;
 
 
 interface ERC721 {
-    // Required methods
     function totalSupply() external view returns (uint256 total);
     function balanceOf(address _owner) external view returns (uint256 balance);
     function ownerOf(uint256 _tokenId) external view returns (address owner);
@@ -10,57 +11,35 @@ interface ERC721 {
     function transfer(address _to, uint256 _tokenId) external;
     function transferFrom(address _from, address _to, uint256 _tokenId) external;
 
-    // Events
     event Transfer(address from, address to, uint256 tokenId);
     event Approval(address owner, address approved, uint256 tokenId);
-
-    // Optional
-    // function name() public view returns (string name);
-    // function symbol() public view returns (string symbol);
-    // function tokensOfOwner(address _owner) external view returns (uint256[] tokenIds);
-    // function tokenMetadata(uint256 _tokenId, string _preferredTransport) public view returns (string infoUrl);
-
-    // ERC-165 Compatibility (https://github.com/ethereum/EIPs/issues/165)
-    function supportsInterface(bytes4 _interfaceID) external view returns (bool);
 }
 
-
-interface ERC721Metadata {
-    function getMetadata(uint256 _tokenId, string memory _preferredTransport) external view returns (string memory buffer) ;
+interface IERC165 {
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
 }
 
-
-abstract contract KittyBase is ERC721 {
-    /*** STORAGE ***/
-
-    /// @dev A mapping from cat IDs to the address that owns them. All cats have
-    ///  some valid owner address, even gen0 cats are created with a non-zero owner.
+abstract contract KittyBase is ERC721, IERC165 {
     mapping (uint256 => address) public kittyIndexToOwner;
 
     // @dev A mapping from owner address to count of tokens that address owns.
-    //  Used internally inside balanceOf() to resolve ownership count.
     mapping (address => uint256) ownershipTokenCount;
 
     /// @dev A mapping from KittyIDs to an address that has been approved to call
-    ///  transferFrom(). Each Kitty can only have one approved address for transfer
-    ///  at any time. A zero value means no approval is outstanding.
+    ///  transferFrom().
     mapping (uint256 => address) public kittyIndexToApproved;
 
     uint256 internal _totalSupply;
 
     /// @dev Assigns ownership of a specific Kitty to an address.
     function _transfer(address _from, address _to, uint256 _tokenId) internal {
-        // Since the number of kittens is capped to 2^32 we can't overflow this
         ownershipTokenCount[_to]++;
-        // transfer ownership
         kittyIndexToOwner[_tokenId] = _to;
         // When creating new kittens _from is 0x0, but we can't account that address.
         if (_from != address(0)) {
             ownershipTokenCount[_from]--;
-            // clear any previously approved ownership exchange
             delete kittyIndexToApproved[_tokenId];
         }
-        // Emit the transfer event.
         emit Transfer(_from, _to, _tokenId);
     }
 
@@ -68,12 +47,7 @@ abstract contract KittyBase is ERC721 {
     string public constant name = "CKMock";
     string public constant symbol = "CKM";
 
-    // The contract that will return kitty metadata
-    ERC721Metadata public erc721Metadata;
-
-    bytes4 constant InterfaceSignature_ERC165 =
-        bytes4(keccak256('supportsInterface(bytes4)'));
-
+    // We use this instead of type(ERC721)interfaceId to be compliant with CK
     bytes4 constant InterfaceSignature_ERC721 =
         bytes4(keccak256('name()')) ^
         bytes4(keccak256('symbol()')) ^
@@ -86,161 +60,80 @@ abstract contract KittyBase is ERC721 {
         bytes4(keccak256('tokensOfOwner(address)')) ^
         bytes4(keccak256('tokenMetadata(uint256,string)'));
 
-    /// @notice Introspection interface as per ERC-165 (https://github.com/ethereum/EIPs/issues/165).
-    ///  Returns true for any standardized interfaces implemented by this contract. We implement
-    ///  ERC-165 (obviously!) and ERC-721.
     function supportsInterface(bytes4 _interfaceID) external pure override returns (bool)
     {
-        // DEBUG ONLY
-        //require((InterfaceSignature_ERC165 == 0x01ffc9a7) && (InterfaceSignature_ERC721 == 0x9a20483d));
-
-        return ((_interfaceID == InterfaceSignature_ERC165) || (_interfaceID == InterfaceSignature_ERC721));
+        return _interfaceID == type(IERC165).interfaceId || _interfaceID == InterfaceSignature_ERC721;
     }
-
-    /// @dev Set the address of the sibling contract that tracks metadata.
-    ///  CEO only.
-    function setMetadataAddress(address _contractAddress) public {
-        erc721Metadata = ERC721Metadata(_contractAddress);
-    }
-
-    // Internal utility functions: These functions all assume that their input arguments
-    // are valid. We leave it to public methods to sanitize their inputs and follow
-    // the required logic.
 
     /// @dev Checks if a given address is the current owner of a particular Kitty.
-    /// @param _claimant the address we are validating against.
-    /// @param _tokenId kitten id, only valid when > 0
     function _owns(address _claimant, uint256 _tokenId) internal view returns (bool) {
         return kittyIndexToOwner[_tokenId] == _claimant;
     }
 
     /// @dev Checks if a given address currently has transferApproval for a particular Kitty.
-    /// @param _claimant the address we are confirming kitten is approved for.
-    /// @param _tokenId kitten id, only valid when > 0
     function _approvedFor(address _claimant, uint256 _tokenId) internal view returns (bool) {
         return kittyIndexToApproved[_tokenId] == _claimant;
     }
 
     /// @dev Marks an address as being approved for transferFrom(), overwriting any previous
     ///  approval. Setting _approved to address(0) clears all transfer approval.
-    ///  NOTE: _approve() does NOT send the Approval event. This is intentional because
-    ///  _approve() and transferFrom() are used together for putting Kitties on auction, and
-    ///  there is no value in spamming the log with Approval events in that case.
     function _approve(uint256 _tokenId, address _approved) internal {
         kittyIndexToApproved[_tokenId] = _approved;
     }
 
     /// @notice Returns the number of Kitties owned by a specific address.
-    /// @param _owner The owner address to check.
-    /// @dev Required for ERC-721 compliance
     function balanceOf(address _owner) public view override returns (uint256 count) {
         return ownershipTokenCount[_owner];
     }
 
-    /// @notice Transfers a Kitty to another address. If transferring to a smart
-    ///  contract be VERY CAREFUL to ensure that it is aware of ERC-721 (or
-    ///  CryptoKitties specifically) or your Kitty may be lost forever. Seriously.
-    /// @param _to The address of the recipient, can be a user or contract.
-    /// @param _tokenId The ID of the Kitty to transfer.
-    /// @dev Required for ERC-721 compliance.
-    function transfer(
-        address _to,
-        uint256 _tokenId
-    )
-        external
-        override
-    {
-        // Safety check to prevent against an unexpected 0x0 default.
+    /// @notice Transfers a Kitty to another address.
+    function transfer(address _to, uint256 _tokenId) external override {
         require(_to != address(0));
         // Disallow transfers to this contract to prevent accidental misuse.
-        // The contract should never own any kitties (except very briefly
-        // after a gen0 cat is created and before it goes on auction).
         require(_to != address(this));
 
         // You can only send your own cat.
         require(_owns(msg.sender, _tokenId));
 
-        // Reassign ownership, clear pending approvals, emit Transfer event.
         _transfer(msg.sender, _to, _tokenId);
     }
 
     /// @notice Grant another address the right to transfer a specific Kitty via
     ///  transferFrom(). This is the preferred flow for transfering NFTs to contracts.
-    /// @param _to The address to be granted transfer approval. Pass address(0) to
-    ///  clear all approvals.
-    /// @param _tokenId The ID of the Kitty that can be transferred if this call succeeds.
-    /// @dev Required for ERC-721 compliance.
-    function approve(
-        address _to,
-        uint256 _tokenId
-    )
-        external
-        override
-    {
+    function approve(address _to, uint256 _tokenId) external override {
         // Only an owner can grant transfer approval.
         require(_owns(msg.sender, _tokenId));
 
-        // Register the approval (replacing any previous approval).
         _approve(_tokenId, _to);
 
-        // Emit approval event.
         emit Approval(msg.sender, _to, _tokenId);
     }
 
     /// @notice Transfer a Kitty owned by another address, for which the calling address
     ///  has previously been granted transfer approval by the owner.
-    /// @param _from The address that owns the Kitty to be transfered.
-    /// @param _to The address that should take ownership of the Kitty. Can be any address,
-    ///  including the caller.
-    /// @param _tokenId The ID of the Kitty to be transferred.
-    /// @dev Required for ERC-721 compliance.
-    function transferFrom(
-        address _from,
-        address _to,
-        uint256 _tokenId
-    )
-        external
-        override
-    {
-        // Safety check to prevent against an unexpected 0x0 default.
+    function transferFrom(address _from, address _to, uint256 _tokenId) external override {
         require(_to != address(0));
         // Disallow transfers to this contract to prevent accidental misuse.
-        // The contract should never own any kitties (except very briefly
-        // after a gen0 cat is created and before it goes on auction).
         require(_to != address(this));
-        // Check for approval and valid ownership
         require(_approvedFor(msg.sender, _tokenId));
         require(_owns(_from, _tokenId));
 
-        // Reassign ownership (also clears pending approvals and emits Transfer event).
         _transfer(_from, _to, _tokenId);
     }
 
     /// @notice Returns the total number of Kitties currently in existence.
-    /// @dev Required for ERC-721 compliance.
     function totalSupply() public view override returns (uint) {
         return _totalSupply;
     }
 
     /// @notice Returns the address currently assigned ownership of a given Kitty.
-    /// @dev Required for ERC-721 compliance.
-    function ownerOf(uint256 _tokenId)
-        external
-        view
-        override
-        returns (address owner)
-    {
+    function ownerOf(uint256 _tokenId) external view override returns (address owner) {
         owner = kittyIndexToOwner[_tokenId];
 
         require(owner != address(0));
     }
 
     /// @notice Returns a list of all Kitty IDs assigned to an address.
-    /// @param _owner The owner whose Kitties we are interested in.
-    /// @dev This method MUST NEVER be called by smart contract code. First, it's fairly
-    ///  expensive (it walks the entire Kitty array looking for cats belonging to owner),
-    ///  but it also returns a dynamic array, which is only supported for web3 calls, and
-    ///  not contract-to-contract calls.
     function tokensOfOwner(address _owner) external view returns(uint256[] memory ownerTokens) {
         uint256 tokenCount = balanceOf(_owner);
 
@@ -252,8 +145,6 @@ abstract contract KittyBase is ERC721 {
             uint256 totalCats = totalSupply();
             uint256 resultIndex = 0;
 
-            // We count on the fact that all cats have IDs starting at 1 and increasing
-            // sequentially up to the totalCat count.
             uint256 catId;
 
             for (catId = 1; catId <= totalCats; catId++) {
@@ -266,19 +157,14 @@ abstract contract KittyBase is ERC721 {
             return result;
         }
     }
-
-    /// @notice Returns a URI pointing to a metadata package for this token conforming to
-    ///  ERC-721 (https://github.com/ethereum/EIPs/issues/721)
-    /// @param _tokenId The ID number of the Kitty whose metadata should be returned.
-    function tokenMetadata(uint256 _tokenId, string calldata _preferredTransport) external view returns (string memory) {
-        require(address(erc721Metadata) != address(0));
-        return erc721Metadata.getMetadata(_tokenId, _preferredTransport);
-    }
 }
 
 
 contract CryptoKittiesMock is KittyBase {
 
+    /**
+     * @notice You can mint any tokenId > 0
+     */
     function mint(uint256 _tokenId, address _to) public {
         require(kittyIndexToOwner[_tokenId] == address(0), "CryptoKittiesMock: tokenId already taken");
         require(_tokenId > 0, "CryptoKittiesMock: cannot mint 0 tokenId");
